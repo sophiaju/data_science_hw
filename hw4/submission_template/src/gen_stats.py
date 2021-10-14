c1edf6de86692d626a20bbc1f7ac9b84fa6e0131
Sophia Ju <sophia.ju10@gmail.com>
2021-10-14 01:24:03 -0400

    newest gen stats script, add ip address and jupyter log files

--- hw4/submission_template/src/gen_stats.py
+++ hw4/submission_template/src/gen_stats.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from pathlib import Path
 import os, sys
-import json
 import argparse, datetime
 parentdir = Path(__file__).parents[1]
 sys.path.append(parentdir)
@@ -22,10 +21,11 @@ def main():
 
     # gets path to the data csv
     data_path = os.path.join(parentdir, 'data', data_file)
-    df = pd.read_csv(data_path, names=['id','opened','closed','zip'], header=None)
-
-    # make zip into ints
-    df['zip'] = df['zip'].astype(int)
+    #df = pd.read_csv(data_path, names=['id','opened','closed','zip'], header=None, dtype={'zip':str})
+    cols = ['id','opened','closed','zip']
+    dtypes = {'id':'str', 'opened':'str', 'closed':'str', 'zip':'str'}
+    parse_dates = ['opened', 'closed']
+    df = pd.read_csv(data_path, sep=',', names=cols, dtype=dtypes, parse_dates=parse_dates, header=None)
 
     # find incident duration for each row and convert to hours
     df['duration'] = (pd.to_datetime(df['closed'])-pd.to_datetime(df['opened'])).astype('timedelta64[h]')
@@ -38,25 +38,15 @@ def main():
 
     # group by month to get all 2020 data
     month = df.groupby(['month'])['duration'].mean()
-    print(month)
     month.to_csv("all_2020.csv", index=True)
     print('file processed by month saved')
 
     # group by month and zipcode
     zip_month = df.groupby(['zip','month'])['duration'].mean().reset_index()
-    print(zip_month)
 
     # save zip, month, avg, to csv
     zip_month.to_csv(output_file, index=False)
-
-    # output path
-    # out_path = os.path.join(parentdir, output_file)
-    # # produce a JSON file
-    # with open (out_path, 'w') as outfile:
-    #     json.dump(zip_month, outfile, indent=4)
-    # print('file processed by zip and month saved')
-
-
+    print('file processed by zip and month saved')
 
 
 if __name__ == '__main__':
