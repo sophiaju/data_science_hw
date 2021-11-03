d29465f041317e4df10348d1d4d73d74d032f881
Sophia Ju <sophia.ju10@gmail.com>
2021-11-02 23:55:52 -0400

    finished first part of hw6

---
+++ hw6/submission_template/src/collect.py
@@ -0,0 +1,41 @@
+import requests, json
+
+# function to generate json of posts given a list of subreddits and the path of output
+def gen_json(list_subs, output_name):
+
+    with open(output_name, 'w') as output:
+
+        for sub in list_subs:
+
+            url = f"https://www.reddit.com/r/{sub}/new.json?limit=100"
+            print(url)
+
+            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
+
+            # get post json
+            r = requests.get(url, headers=headers)
+            root_element = r.json()
+            posts = root_element['data']['children']
+
+            # write posts to output file
+            for post in posts:
+                json.dump(post, output)
+                output.write("\n")
+
+
+def main():
+    # sample 1 subreddits
+    pop_subs = ["funny", "AskReddit", "gaming", "aww", "pics", "Music", "science", "worldnews","videos", "todayilearned"]
+
+    # sample 2 subreddits
+    pop_posts = ["AskReddit", "memes", "politics", "nfl", "nba", "wallstreetbets", "teenagers", "PublicFreakout", "leagueoflegends", "unpopularopinion"]
+
+    # generate files
+    gen_json(pop_subs, "../sample1.json")
+    gen_json(pop_posts, "../sample2.json")
+    
+        
+
+if __name__ == '__main__':
+    main()
+
---
+++ hw6/submission_template/src/compute_title_lengths.py
@@ -0,0 +1,36 @@
+import json
+import argparse
+
+def main():
+
+    # get input path
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help = "add the name of the input file", action="store")
+    args = parser.parse_args()
+    input_path = args.input
+
+    with open(input_path, 'r') as infile:
+        
+        total_words = 0
+        total_posts = 0
+
+        # line by line, 
+        for line in infile:
+            post = json.loads(line)
+            title = post['data']['title']
+            # print(title, len(title.split()))
+            
+            # add the number of words in that title to total words
+            total_words += len(title.split())
+            # count the number of posts
+            total_posts += 1
+            
+        # print(total_words, total_posts)
+
+        # calculate avg title length and print!
+        avg_title_len = total_words/total_posts
+        print(avg_title_len)
+
+        
+if __name__ == '__main__':
+    main()
